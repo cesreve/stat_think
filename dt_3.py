daabc6150f62b1d3b41db27bfd71e11fa7a8ddd2
cesreve <cesreve@gmail.com>
2019-12-17 16:20:42 +0100

    Fin partie 1

--- dt_3.py
+++ dt_3.py
@@ -11,6 +11,23 @@ import seaborn as sns
 sns.set()
 
 # =============================================================================
+# ECDF
+# =============================================================================
+
+def ecdf(data):
+    """Compute ECDF for a one-dimensional array of measurements."""
+    # Number of data points: n
+    n = len(data)
+
+    # x-data for the ECDF: x
+    x = np.sort(data)
+
+    # y-data for the ECDF: y
+    y = np.arange(1, n+1) / n
+
+    return x, y
+
+# =============================================================================
 # No ideai what I am doing
 # =============================================================================
 
@@ -53,3 +70,47 @@ _ = plt.legend(('std = 1', 'std = 3', 'std = 10'))
 plt.ylim(-0.01, 0.42)
 plt.show()
 plt.close('all')
+
+# =============================================================================
+# Corresponding CDF
+# =============================================================================
+# Generate CDFs
+x_std1, y_std1 = ecdf(samples_std1)
+x_std3, y_std3 = ecdf(samples_std3)
+x_std10, y_std10 = ecdf(samples_std10)
+
+# Plot CDFs
+_ = plt.plot(x_std1, y_std1, marker = '.', linestyle = 'none')
+_ = plt.plot(x_std3, y_std3, marker = '.', linestyle = 'none')
+_ = plt.plot(x_std10, y_std10, marker = '.', linestyle = 'none')
+
+# Make a legend and show the plot
+_ = plt.legend(('std = 1', 'std = 3', 'std = 10'), loc='lower right')
+plt.show()
+
+# =============================================================================
+# Exponential distribution
+# =============================================================================
+def successive_poisson(tau1, tau2, size=1):
+    """Compute time for arrival of 2 successive Poisson processes."""
+    # Draw samples out of first exponential distribution: t1
+    t1 = np.random.exponential(tau1, size)
+
+    # Draw samples out of second exponential distribution: t2
+    t2 = np.random.exponential(tau2, size)
+
+    return t1 + t2
+
+# Draw samples of waiting times: waiting_times
+waiting_times = successive_poisson(764, 715, 100000)
+
+# Make the histogram
+_ = plt.hist(waiting_times, bins=100, normed=True, histtype='step')
+
+# Label axes
+_ = plt.xlabel('waiting_times')
+_ = plt.ylabel('PDF')
+
+
+# Show the plot
+plt.show()
